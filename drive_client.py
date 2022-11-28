bef5a1c001231c45965d7fd7165012c706d7bedb
Huy Nguyen <phuochuy.nguyen@shodan.io>
2022-11-29 01:32:58 +0700

    update

--- drive_client.py
+++ drive_client.py
@@ -118,12 +118,12 @@ def read_file_in_chunk(token, client_id, client_secret, filename, offset, length
         service = build('drive', 'v3', credentials=creds)
         file_id, real_offset = get_child_id_hold_data(filename, offset, length)
         request = service.files().get_media(fileId=file_id)
-        request.headers["Range"] = "bytes={}-{}".format(real_offset, real_offset + length)
+        request.headers["Range"] = "bytes={}-{}".format(real_offset, real_offset + length - 1)
         if offset < 64 * 1024:
             # request.headers["Range"] = "bytes={}-{}".format(0, 64 * 1024 * 1024)
             request.headers["Range"] = ""
             fh = io.BytesIO(request.execute())
-            return FERNET_OBJ.decrypt(fh.getvalue())[:length - offset]
+            return FERNET_OBJ.decrypt(fh.getvalue())[:length - 1 - offset]
         fh = io.BytesIO(request.execute())
 
     except Exception as error:
